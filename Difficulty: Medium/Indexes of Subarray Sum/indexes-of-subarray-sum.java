1e271f3d10a242a264b5dce73e746fbc86528aec
Aditya kukade <127621475+adityakukade32@users.noreply.github.com>
2024-10-09 22:38:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        StringBuilder sb = new StringBuilder();
+        for (int e : a) sb.append(e + " ");
+        System.out.println(sb);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String str[] = br.readLine().trim().split(" ");
+
+            int n = Integer.parseInt(str[0]);
+            int s = Integer.parseInt(str[1]);
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(arr, n, s);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        // code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        int curSum=0;
+        int left=0;
+        for(int i=0;i<n;i++)
+        {
+            curSum+=arr[i];
+            while(curSum>s && left<i)
+            {
+                curSum-=arr[left];
+                left++;
+                
+            }
+            if(curSum==s)
+            {
+                ans.add(left+1);
+                ans.add(i+1);
+                return ans;
+                
+            }
+            // if(ans.size()==0)ans.add(-1);
+            
+            
+        }
+        ans.add(-1);
+        return ans;
+    }
+}
